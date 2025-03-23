559aad1ee28fdce269113b4e459d0a8fa6fd173a
Ranjeet3345 <yadavranjeet7770@gmail.com>
2025-03-23 18:48:12 +0530

    Now Started Again

--- CurrentAffairs/wsgi.py
+++ CurrentAffairs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for app project.
+WSGI config for CurrentAffairs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CurrentAffairs.settings')
 
 application = get_wsgi_application()
